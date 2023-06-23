caf45f42114a8225e540e807cc61a90c3faba94c
Veliko <Kardziev>
2023-06-23 11:35:10 +0200

    add first test

--- components/ArtPieces/ArtPieces.test.js
+++ components/ArtPieces/ArtPieces.test.js
@@ -3,6 +3,6 @@ import ArtPieces from ".";
 
 test("renders list elements", () => {
   render(<ArtPieces></ArtPieces>);
-  const listitem = screen.getAllByRole("listitem");
+  const listitem = screen.getAllByRole("listitem", {});
   expect(listitem).toBeInTheDocument();
 });
--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import useSWR from "swr";
 const url = "https://example-apis.vercel.app/api/art";
 const fetcher = async (url) => {
   const res = await fetch(url);
-
   // If the status code is not in the range 200-299,
   // we still try to parse and throw it.
   if (!res.ok) {
