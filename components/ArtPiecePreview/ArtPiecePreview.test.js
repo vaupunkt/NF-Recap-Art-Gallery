e5566e92e0bb4f825010e051bdf9aea0fe84cdd6
Veliko <Kardziev>
2023-06-26 11:58:06 +0200

    Tests for Userstory 1

---
+++ components/ArtPiecePreview/ArtPiecePreview.test.js
@@ -0,0 +1,86 @@
+import { render, screen } from "@testing-library/react";
+import ArtPiecesPreview from ".";
+
+test("Artpiece image is displayed", () => {
+  const piece = {
+    slug: "orange-red-and-green",
+    artist: "Steve Johnson",
+    name: "Orange Red and Green Abstract Painting",
+    imageSource:
+      "https://example-apis.vercel.app/assets/art/orange-red-and-green.jpg",
+    year: "2018",
+    genre: "Abstract Painting",
+    colors: ["#0f5855", "#e6ba15", "#b42011", "#cec4c6", "#d5561f"],
+    dimensions: {
+      height: 2432,
+      width: 1920,
+      type: "jpg",
+    },
+  };
+  render(
+    <ArtPiecesPreview
+      image={piece.imageSource}
+      title={piece.name}
+      artist={piece.artist}
+      slug={piece.slug}
+    />
+  );
+  const image = screen.getByRole("img");
+  expect(image).toBeInTheDocument();
+});
+
+test("Artpiece title is displayed", () => {
+  const piece = {
+    slug: "orange-red-and-green",
+    artist: "Steve Johnson",
+    name: "Orange Red and Green Abstract Painting",
+    imageSource:
+      "https://example-apis.vercel.app/assets/art/orange-red-and-green.jpg",
+    year: "2018",
+    genre: "Abstract Painting",
+    colors: ["#0f5855", "#e6ba15", "#b42011", "#cec4c6", "#d5561f"],
+    dimensions: {
+      height: 2432,
+      width: 1920,
+      type: "jpg",
+    },
+  };
+  render(
+    <ArtPiecesPreview
+      image={piece.imageSource}
+      title={piece.name}
+      artist={piece.artist}
+      slug={piece.slug}
+    />
+  );
+  const title = screen.getByRole("heading", { name: piece.title });
+  expect(title).toBeInTheDocument();
+});
+
+test("Artpiece artist is displayed", () => {
+  const piece = {
+    slug: "orange-red-and-green",
+    artist: "Steve Johnson",
+    name: "Orange Red and Green Abstract Painting",
+    imageSource:
+      "https://example-apis.vercel.app/assets/art/orange-red-and-green.jpg",
+    year: "2018",
+    genre: "Abstract Painting",
+    colors: ["#0f5855", "#e6ba15", "#b42011", "#cec4c6", "#d5561f"],
+    dimensions: {
+      height: 2432,
+      width: 1920,
+      type: "jpg",
+    },
+  };
+  render(
+    <ArtPiecesPreview
+      image={piece.imageSource}
+      title={piece.name}
+      artist={piece.artist}
+      slug={piece.slug}
+    />
+  );
+  const artist = screen.getByText(piece.artist);
+  expect(artist).toBeInTheDocument();
+});
